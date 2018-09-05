6572f9fbd5dfbb627896855a4a644aae06b2e28f
Jordan Guy <jordan.guy.277@gmail.com>
2018-09-05 13:42:39 -0400

    initializing redux

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Harry Potter'},
+        { title: 'JS the good parts'},
+        { title: 'The Hobbit'},
+        { title: 'Game of Thrones'}
+    ]
+}
